416910168ec48e6dfc9bdada83f2cc6d2376c846
Alexander <aleksand-isaev@yandex.ru>
2018-01-25 13:52:49 +0400

    Headers were added to requests

--- src/verify-ts-client.ts
+++ src/verify-ts-client.ts
@@ -1,6 +1,6 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
+// import "core-js/fn/array.find"
+// ...
 
 import * as WebRequest from 'web-request';
 import VerifyService from './interfaces/verify-service';
@@ -13,10 +13,17 @@ import ValidationData from './validation-data/validation-data';
  * Class VerifyClient
  */
 export default class VerifyClient implements VerifyService {
-  constructor() {
-    //TODO: send headers
-    WebRequest.defaults({throwResponseError: true});
+  constructor(jwtToken: string) {
+    WebRequest.defaults({
+      throwResponseError: true,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${jwtToken}`,
+        'Accept': 'application/vnd.jincor+json; version=1'
+      }
+    });
   }
+  
   /**
      * Initiate verification process
      *
@@ -26,71 +33,67 @@ export default class VerifyClient implements VerifyService {
      *
      * @throws {Error}
      */
-    public async initiate(method: VerificationMethod): Promise<VerificationDetails>
-    {
-        const response = await WebRequest.post(
-          `/methods/${method.getMethodType()}/actions/initiate`,
-          method.getRequestParameters()
-        )
+  public async initiate(method: VerificationMethod): Promise<VerificationDetails> {
+    const response = await WebRequest.post(
+      `/methods/${method.getMethodType()}/actions/initiate`,
+      method.getRequestParameters()
+    )
 
-        return VerificationDetailsCreator.create(method.getMethodType(), response.content);
-    }
+    return VerificationDetailsCreator.create(method.getMethodType(), response.content);
+  }
 
-    /**
-     * Validate the code
-     *
-     * @param {ValidationData} data Validation Data
-     *
-     * @return {VerificationResult}
-     *
-     * @throws {Error}
-     */
-    public async validate(data: ValidationData): Promise<VerificationResult>
-    {
-      try {
-        const response = await WebRequest.post(
-          `/methods/${data.getMethodType()}/verifiers/${data.getVerificationId()}/actions/validate`,
-          data.getRequestParameters()
-        )
-
-        return VerificationResult.fromJson(response.content);
-      } catch (exception) {
-        if (exception.statusCode === 422) {
-          throw new Error('Invalid Code');
-        }
-
-        throw exception;
-      }
-    }
-
-    /**
-     * Invalidate the code
-     *
-     * @param {InvalidationData} data Invalidation Data
-     *
-     * @return {boolean}
-     */
-    public async invalidate(data: InvalidationData): Promise<boolean>
-    {
-      const response = await WebRequest.delete(
-        `/methods/${data.getMethodType()}/verifiers/${data.getVerificationId()}`
-      );
-
-      return true;
-    }
-
-    /**
-     * @param {string} verificationId
-     * @param {string} methodType
-     *
-     * @return {VerificationDetails}
-     */
-    public async getVerification(verificationId: string, methodType: string): Promise<VerificationDetails>
-    {
-      const response = await WebRequest.get(
-        `/methods/${methodType}/verifiers/${verificationId}`
+  /**
+   * Validate the code
+   *
+   * @param {ValidationData} data Validation Data
+   *
+   * @return {VerificationResult}
+   *
+   * @throws {Error}
+   */
+  public async validate(data: ValidationData): Promise<VerificationResult> {
+    try {
+      const response = await WebRequest.post(
+        `/methods/${data.getMethodType()}/verifiers/${data.getVerificationId()}/actions/validate`,
+        data.getRequestParameters()
       )
 
-      return VerificationDetailsCreator.create(methodType, response.content);
+      return VerificationResult.fromJson(response.content);
+    } catch (exception) {
+      if (exception.statusCode === 422) {
+        throw new Error('Invalid Code');
+      }
+
+      throw exception;
     }
+  }
+
+  /**
+   * Invalidate the code
+   *
+   * @param {InvalidationData} data Invalidation Data
+   *
+   * @return {boolean}
+   */
+  public async invalidate(data: InvalidationData): Promise<boolean> {
+    const response = await WebRequest.delete(
+      `/methods/${data.getMethodType()}/verifiers/${data.getVerificationId()}`
+    );
+
+    return true;
+  }
+
+  /**
+   * @param {string} verificationId
+   * @param {string} methodType
+   *
+   * @return {VerificationDetails}
+   */
+  public async getVerification(verificationId: string, methodType: string): Promise<VerificationDetails> {
+    const response = await WebRequest.get(
+      `/methods/${methodType}/verifiers/${verificationId}`
+    )
+
+    return VerificationDetailsCreator.create(methodType, response.content);
+  }
 }
