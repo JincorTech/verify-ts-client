220a23377033aaa2e49edea8ec55b3a09c2e0076
Alexander <aleksand-isaev@yandex.ru>
2018-01-27 19:36:38 +0400

    Index.ts was added

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { VerifyClient } from './client/verify-client';
+export { default as EmailInitiateData } from './requests/initiate/email-initiate-data';
+export { default as GoogleAuthInitiateData } from './requests/initiate/google-auth-initiate-data';
+export { InitiateData, VerificationTypes } from './requests/initiate/initiate-data';
